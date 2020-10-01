01b677111e626a97bf64d66ea7b5950e619862b6
samer-repos <samer.fawadleh@hotmail.com>
2020-10-01 21:25:39 +0300

    Replace recursion with a queue and a while loop, code refactoring

--- TravelingKnight/main.js
+++ TravelingKnight/main.js
@@ -1,138 +1,126 @@
 const BOARD_SIZE = 3;
+const MAX_BOARD_COORDINATE = BOARD_SIZE - 1;
 const STARTING_POINT = {
   x: 0,
   y: 0,
 };
-const BOARD = [];
-const POSSIBLE_MOVEMENTS = [];
 
 const run = () => {
-  const isValidStartingPosition = isValidPosition(STARTING_POINT);
-  if (!isValidStartingPosition) {
+  if (!isValidPosition(STARTING_POINT)) {
     console.log("Invalid starting position!\n", []);
     return;
   }
 
-  //fill the board with 0s => 0 means cell is not visited
-  for (let i = 0; i < BOARD_SIZE; i++) {
-    BOARD.push([]);
-    for (let j = 0; j < BOARD_SIZE; j++) {
-      BOARD[i].push(0);
+  const result = getResult(STARTING_POINT);
+  result.forEach((item, index) => {
+    console.log(`Path #${index + 1}:`);
+
+    item.forEach((position, index) =>
+      console.log(`\t ${index + 1}:`, position)
+    );
+  });
+};
+
+const getResult = (position) => {
+  const queue = [position];
+  const result = [];
+
+  while (queue.length > 0) {
+    const currentPosition = queue.shift();
+    const { x, y } = currentPosition;
+
+    const nextPossibleMovements = getPossibleMovements(currentPosition);
+    const nextValidMovements = nextPossibleMovements.filter((move) =>
+      isValidPosition(move)
+    );
+
+    let isEndOfPath = true;
+    nextValidMovements.forEach((move) => {
+      const path = currentPosition.path ? [...currentPosition.path] : [];
+
+      //if the result is -1 => cell was not visited
+      const indexOfNextMove = path.findIndex(
+        (point) => point.x === move.x && point.y === move.y
+      );
+
+      //if cell was not visited
+      if (indexOfNextMove < 0) {
+        isEndOfPath = false;
+        path.push({ x, y });
+        queue.push({ ...move, path });
+      }
+    });
+
+    //end of path => add path to result
+    if (isEndOfPath) {
+      //path is undefined when the full path is just the starting position (e.g. BOARD_SIZE = 2)
+      if (!currentPosition.path) {
+        currentPosition.path = [];
+      }
+      currentPosition.path.push({ x, y });
+      result.push(currentPosition.path);
     }
   }
-
-  let currentPositionX = STARTING_POINT.x;
-  let currentPositionY = STARTING_POINT.y;
-
-  getResult(currentPositionX, currentPositionY);
-  POSSIBLE_MOVEMENTS.forEach((item, index) => {
-    console.log(`Path #${index + 1}:`);
-    item.forEach((position) => console.log("\t", position));
-  });
+  return result;
 };
 
-//recursion to calculate the result
-const getResult = (currentPositionX, currentPositionY, movements = []) => {
-  const nextPossibleMovements = getPossibleMovements(
-    currentPositionX,
-    currentPositionY
-  );
-  const validMovements = getValidMovements(nextPossibleMovements);
-  debugger;
-
-  //mark current cell as visited
-  BOARD[currentPositionX][currentPositionY] = 1;
-
-  //add current cell to current movements
-  const movementsCloned = [...movements];
-  movementsCloned.push({ x: currentPositionX, y: currentPositionY });
-
-  validMovements.forEach((move) => {
-    const { x, y } = move;
-
-    getResult(x, y, movementsCloned);
-  });
-
-  //when there's no more moves => this is the full path
-  if (validMovements.length <= 0) {
-    POSSIBLE_MOVEMENTS.push(movementsCloned);
-  }
-  BOARD[currentPositionX][currentPositionY] = 0;
-};
-
-const getPossibleMovements = (currentPositionX, currentPositionY) => {
+const getPossibleMovements = (position) => {
+  const { x, y } = position;
   const possibleMovements = [];
 
-  //south east
+  //north west
   possibleMovements.push({
-    x: currentPositionX + 2,
-    y: currentPositionY + 1,
+    x: x - 2,
+    y: y - 1,
   });
 
   possibleMovements.push({
-    x: currentPositionX + 1,
-    y: currentPositionY + 2,
+    x: x - 1,
+    y: y - 2,
   });
 
   //north east
   possibleMovements.push({
-    x: currentPositionX + 2,
-    y: currentPositionY - 1,
+    x: x + 2,
+    y: y - 1,
   });
 
   possibleMovements.push({
-    x: currentPositionX + 1,
-    y: currentPositionY - 2,
+    x: x + 1,
+    y: y - 2,
   });
 
   //south west
   possibleMovements.push({
-    x: currentPositionX - 2,
-    y: currentPositionY + 1,
+    x: x - 2,
+    y: y + 1,
   });
 
   possibleMovements.push({
-    x: currentPositionX - 1,
-    y: currentPositionY + 2,
+    x: x - 1,
+    y: y + 2,
   });
 
-  //north west
+  //south east
   possibleMovements.push({
-    x: currentPositionX - 2,
-    y: currentPositionY - 1,
+    x: x + 2,
+    y: y + 1,
   });
 
   possibleMovements.push({
-    x: currentPositionX - 1,
-    y: currentPositionY - 2,
+    x: x + 1,
+    y: y + 2,
   });
 
   return possibleMovements;
 };
 
-const getValidMovements = (possibleMovements) => {
-  const validMovements = [];
-
-  for (let i = 0; i < possibleMovements.length; i++) {
-    const move = possibleMovements[i];
-
-    //if in range and the cell is not visited
-    if (isValidPosition(move) && !BOARD[move.x][move.y]) {
-      validMovements.push(move);
-    }
-  }
-
-  return validMovements;
-};
-
 //check if position is within the board range
 const isValidPosition = (point) => {
-  const maxCoordinatePoint = BOARD_SIZE - 1;
-
   if (
-    point.x > maxCoordinatePoint ||
+    point.x > MAX_BOARD_COORDINATE ||
     point.x < 0 ||
-    point.y > maxCoordinatePoint ||
+    point.y > MAX_BOARD_COORDINATE ||
     point.y < 0
   ) {
     return false;
